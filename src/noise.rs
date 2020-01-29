cb47719291524c048b07520c447d708385d0c03e
Franz Heinzmann (Frando) <frando@unbiskant.org>
2020-01-29 20:29:11 +0100

    cleanup

--- src/noise.rs
+++ src/noise.rs
@@ -1,14 +1,14 @@
-use snow::{Builder, Error as SnowError, HandshakeState, Keypair};
-use async_std::prelude::*;
-use std::sync::Arc;
-use async_std::io::{Result,Read,Write,ReadExt,BufReader,BufWriter};
-use std::clone::Clone;
+use async_std::io::{BufReader, BufWriter, Read, ReadExt, Result, Write};
 use async_std::net::TcpStream;
+use async_std::prelude::*;
 use async_std::task::{Context, Poll};
-use std::pin::Pin;
+use snow::{Builder, Error as SnowError, HandshakeState, Keypair};
+use std::clone::Clone;
 use std::convert::TryInto;
+use std::pin::Pin;
+use std::sync::Arc;
 
-pub async fn handshake (stream: TcpStream, is_initiator: bool) -> Result<()> {
+pub async fn handshake(stream: TcpStream, is_initiator: bool) -> Result<()> {
     let stream = CloneableStream(Arc::new(stream));
     let mut reader = BufReader::new(stream.clone());
     let mut writer = BufWriter::new(stream.clone());
@@ -18,9 +18,12 @@ pub async fn handshake (stream: TcpStream, is_initiator: bool) -> Result<()> {
     let mut rx_len;
     let mut tx_len;
 
-    let payload = format!("rusty payload {} init", is_initiator).as_bytes().to_vec();
+    let payload = format!("rusty payload {} init", is_initiator)
+        .as_bytes()
+        .to_vec();
 
     let (mut noise, local_keypair) = build_handshake_state(is_initiator).unwrap();
+
     eprintln!("start handshake (rust)");
     eprintln!("initiator {}", is_initiator);
     eprintln!("loc pk {:x?}", &local_keypair.public);
@@ -36,7 +39,6 @@ pub async fn handshake (stream: TcpStream, is_initiator: bool) -> Result<()> {
     tx_len = noise.write_message(&payload, &mut buf_tx).unwrap();
     write(&mut writer, &buf_tx[..tx_len]).await?;
 
-
     if !is_initiator {
         let msg = read(&mut reader).await?;
         rx_len = noise.read_message(&msg, &mut buf_rx).unwrap();
@@ -45,19 +47,19 @@ pub async fn handshake (stream: TcpStream, is_initiator: bool) -> Result<()> {
     eprintln!("handshake complete!");
     eprintln!("loc pk {:x?}", &local_keypair.public);
     eprintln!("rem pk {:x?}", noise.get_remote_static().unwrap());
-    // eprintln!("remote payload len: {}", remote_payload_len);
-    // let remote_nonce = decode_nonce_msg(&buf_rx[..remote_payload_len]).unwrap();
-    // eprintln!("remote nonce: {:x?}", remote_nonce);
     eprintln!("handshakehash len: {}", noise.get_handshake_hash().len());
     eprintln!("handshakehash: {:x?}", noise.get_handshake_hash());
-    eprintln!("remote payload: {}", String::from_utf8_lossy(&buf_rx[..rx_len]));
-
-    // eprintln!("complete? {}", noise.is_handshake_finished());
+    eprintln!(
+        "remote payload: {}",
+        String::from_utf8_lossy(&buf_rx[..rx_len])
+    );
 
     Ok(())
 }
 
-pub fn build_handshake_state(is_initiator: bool) -> std::result::Result<(HandshakeState, Keypair), SnowError> {
+pub fn build_handshake_state(
+    is_initiator: bool,
+) -> std::result::Result<(HandshakeState, Keypair), SnowError> {
     static PATTERN: &'static str = "Noise_XX_25519_XChaChaPoly_BLAKE2b";
     let builder: Builder<'_> = Builder::new(PATTERN.parse()?);
     let key_pair = builder.generate_keypair().unwrap();
@@ -74,7 +76,7 @@ pub fn build_handshake_state(is_initiator: bool) -> std::result::Result<(Handsha
     Ok((noise, key_pair))
 }
 
-async fn read<R>(reader: &mut BufReader<R>) -> Result<Vec<u8>> 
+async fn read<R>(reader: &mut BufReader<R>) -> Result<Vec<u8>>
 where
     R: Read + Unpin,
 {
@@ -87,7 +89,7 @@ where
     Ok(buf)
 }
 
-async fn write<W>(writer: &mut BufWriter<W>, buf: &[u8]) -> Result<()> 
+async fn write<W>(writer: &mut BufWriter<W>, buf: &[u8]) -> Result<()>
 where
     W: Write + Unpin,
 {
